3d29722ef00844802f0da60f122c11deeccc23c1
Eriza Fazli <erizzaaaaa@gmail.com>
2015-01-30 00:35:12 +0100

    better script

--- chn_or_jpn.py
+++ chn_or_jpn.py
@@ -1,12 +1,12 @@
 import pickle
-import pandas as pd
+from pandas import DataFrame
 from sklearn.base import BaseEstimator, TransformerMixin
 
-nations = ('china', 'japan')
+origin = ('chinese', 'japanese')
 
 class LengthExtractor(BaseEstimator, TransformerMixin):
     def transform(self, X, **transform_params):
-        return pd.DataFrame([len(x) for x in X])
+        return DataFrame([len(x) for x in X])
 
     def fit(self, X, y=None, **fit_params):
         return self
@@ -15,5 +15,10 @@ with open('clf.pkl', 'rb') as f:
     clf = pickle.load(f)
 
 while True:
-    test_name = input('enter a name: ')
-    print(nations[clf.predict([test_name])])
+    try:
+        test_name = input('enter a name: ')
+        print(origin[clf.predict([test_name])])
+    except KeyboardInterrupt:
+        print('\n')
+        break
+
